f2a323ac4aee6ffcf7b050389972b90306907f85
Damian Sopel <dsopel0@gmail.com>
2017-11-07 20:43:38 -0600

    Initial commit

---
+++ js.js
@@ -0,0 +1,42 @@
+var phoneNum = ''
+
+function handleClick() {
+	$('.btn-circle').on('click', function() {
+		var number=$(this).attr("value");
+		phoneNum += number;
+		$('.phone-number').html(phoneNum)
+	})
+}
+
+function handleClear() {
+	$('#clear').on('click', function() {
+		phoneNum = "";
+		$('.phone-number').html(phoneNum)
+	})
+}
+
+function handleBack() {
+	$('.back').on('click', function() {
+		var newNum = phoneNum.slice(0,-1)
+		phoneNum = newNum
+		console.log(newNum)
+		$('.phone-number').html(phoneNum)
+	})
+}
+
+function handleDial() {
+	$('.dial').on('click', function() {
+		$('.alert-success').removeClass('hidden')
+		$('.alert-success').html("You have dialed " + '<strong>' + phoneNum + '</strong>')
+		phoneNum = "";
+		$('.phone-number').html('')
+
+	})
+}
+
+$(document).ready(function() {
+	handleClick();
+	handleClear();
+	handleBack();
+	handleDial();
+})
